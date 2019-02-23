221ff5f2edad72d72e8f80df53e839c812ea8226
Elaina <elainaseki@gmail.com>
2019-02-23 20:18:09 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,9 +2,11 @@ class Hash
   def keys_of(arguments)
     arr = []
     self.each do|animal, place|
-     if place == arguments
+     arguments.each do|i|
+     if place == i
        arr << animal
      end
+    end
      end
      return arr
      # code goes here
